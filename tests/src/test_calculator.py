be4d568eff91b2612c3b036c1520205a460c825c
Jerrylin20 <1211003lin@gmail.com>
2023-02-17 21:40:05 +0900

    added multify

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -10,4 +10,8 @@ def test_add():
 def test_subtract():
     assert cal.subtract(3,1) == 2
     assert cal.subtract(5,2) == 3
+    assert cal.subtract(2,1) == 1
 
+def test_multify():
+    assert cal.mulfify(2,2) == 4
+    assert cal.multify(100,0) == 0
